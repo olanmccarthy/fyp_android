b614588ca42db9887b376864f3ed08badf0691b9
olan <olan@olan.com>
2020-03-02 18:25:10 +0000

    added restapi calls for car makes and models

---
+++ app/src/main/java/com/olan/finalyearproject/helpers/CarMakeSpinnerListener.kt
@@ -0,0 +1,49 @@
+package com.olan.finalyearproject.helpers
+
+import android.app.Activity
+import android.util.Log.d
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import com.google.gson.GsonBuilder
+import com.squareup.okhttp.Callback
+import com.squareup.okhttp.OkHttpClient
+import com.squareup.okhttp.Request
+import com.squareup.okhttp.Response
+import java.io.IOException
+
+class CarMakeSpinnerListener(
+    var carModels: ArrayList<String>, var carModelAdapter: ArrayAdapter<String>, var activity: Activity
+) : AdapterView.OnItemSelectedListener {
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        if(parent?.getItemAtPosition(position) != null){
+            val url = "http://leela.netsoc.co:5001/car_models/${parent.getItemAtPosition(position)}"
+            val request = Request.Builder().url(url).build()
+            val client = OkHttpClient()
+
+            client.newCall(request).enqueue(object: Callback {
+                override fun onFailure(request: Request?, e: IOException?) {
+                    d("olanDebug", "error occured connecting to rest $e")
+                }
+
+                override fun onResponse(response: Response?) {
+                    val body = response?.body()?.string()
+                    val gson = GsonBuilder().create()
+                    val holder = gson.fromJson(body, ModelHolder::class.java)
+                    carModels.clear()
+                    holder.models.forEach { item -> carModels.add(item) }
+                    d("olanDebug", "************ rest response for carmakespinner $body*************")
+                    activity.runOnUiThread { carModelAdapter.notifyDataSetChanged() }
+
+                }
+
+            })
+        }
+    }
+}
+
+class ModelHolder(val models: List<String>)
--- app/src/main/java/com/olan/finalyearproject/ui/fragments/AddActivityFragment.kt
+++ app/src/main/java/com/olan/finalyearproject/ui/fragments/AddActivityFragment.kt
@@ -8,8 +8,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import com.google.gson.GsonBuilder
 import com.olan.finalyearproject.R
+import com.olan.finalyearproject.helpers.CarMakeSpinnerListener
+import com.squareup.okhttp.Callback
+import com.squareup.okhttp.OkHttpClient
+import com.squareup.okhttp.Request
+import com.squareup.okhttp.Response
 import org.w3c.dom.Text
+import java.io.IOException
 
 /**
  * A simple [Fragment] subclass.
@@ -25,11 +32,18 @@ class AddActivityFragment : Fragment(), AdapterView.OnItemSelectedListener {
     lateinit var carMakeSpinner: Spinner
     lateinit var carModelSpinner: Spinner
 
+    lateinit var carMakeAdapter: ArrayAdapter<String>
+    lateinit var carModelAdapter: ArrayAdapter<String>
+
+    var carMakes = arrayListOf("None")
+    var carModels = arrayListOf("None")
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        fetchJson()
         return inflater.inflate(R.layout.fragment_add_activity, container, false)
     }
 
@@ -46,35 +60,47 @@ class AddActivityFragment : Fragment(), AdapterView.OnItemSelectedListener {
         carMakeSpinner = view.findViewById(R.id.carMakeSpinner)
         carModelSpinner = view.findViewById(R.id.carModelSpinner)
 
+        //create adapter for viewing the task type spinner
+        val taskTypeAdapter = ArrayAdapter.createFromResource(
+            activity!!, R.array.TaskTypes, android.R.layout.simple_spinner_dropdown_item
+        )
+        taskTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        taskTypeSpinner.adapter = taskTypeAdapter
+
+
+        val journeyTypeAdapter = ArrayAdapter.createFromResource(
+            activity!!, R.array.JourneyTypes, android.R.layout.simple_spinner_dropdown_item
+        )
+        journeyTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        journeyTypeSpinner.adapter = journeyTypeAdapter
+
+        carMakeAdapter = ArrayAdapter(
+            activity!!, android.R.layout.simple_spinner_dropdown_item, carMakes
+        )
+        carMakeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        carMakeSpinner.adapter = carMakeAdapter
+
+        carModelAdapter  = ArrayAdapter(
+            activity!!, android.R.layout.simple_spinner_dropdown_item, carModels
+        )
+        carModelAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        carModelSpinner.adapter = carModelAdapter
 
         //set the listener for both spinners to be this class
         taskTypeSpinner.onItemSelectedListener = this
         journeyTypeSpinner.onItemSelectedListener = this
-
-        //create adapter for viewing the task type spinner
-        ArrayAdapter.createFromResource(
-            activity!!,
-            R.array.TaskTypes,
-            android.R.layout.simple_spinner_dropdown_item
-        ).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            taskTypeSpinner.adapter = adapter
-        }
-
-        //create the adapter for viewing the journey type spinner
-        ArrayAdapter.createFromResource(
-            activity!!,
-            R.array.JourneyTypes,
-            android.R.layout.simple_spinner_dropdown_item
-        ).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            journeyTypeSpinner.adapter = adapter
-        }
+        carMakeSpinner.onItemSelectedListener = CarMakeSpinnerListener(carModels, carModelAdapter, activity!!)
     }
 
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
         //switch statement controller for task type and journey type spinners
         //TODO add separate event handlers for all the spinners
+
+        //create arrays pointing to each collection of views associated with journey type to simplify
+        //altering visibility
+        val carViews = arrayOf(carMakeSpinner, carModelSpinner)
+        val bikeViews = arrayOf(isElectricSwitch)
+
         when(parent?.getItemAtPosition(position)){
             "JourneyTask" -> {
                 d("olanDebug", "journey task selected")
@@ -85,13 +111,13 @@ class AddActivityFragment : Fragment(), AdapterView.OnItemSelectedListener {
             } //TODO add cases for other task types
             "Bike Journey" ->{
                 d("olanDebug", "bike journey selected")
-                isElectricSwitch.visibility = View.VISIBLE
-
+                carViews.forEach { item -> item.visibility = View.INVISIBLE }
+                bikeViews.forEach { item -> item.visibility = View.VISIBLE }
             }
             "Car Journey" ->{
                 d("olanDebug", "car journey selected")
-                carModelSpinner.visibility = View.VISIBLE
-                carMakeSpinner.visibility = View.VISIBLE
+                carViews.forEach { item -> item.visibility = View.VISIBLE }
+                //call getjson(), update array for car make adapter, call carMakeAdapter.notifyDataSetChanged
 
             }
             "Transit Journey" ->{
@@ -109,4 +135,39 @@ class AddActivityFragment : Fragment(), AdapterView.OnItemSelectedListener {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+    fun fetchJson(){
+        d("olanDebug","running fetch json")
+        val url = "http://leela.netsoc.co:5001/car_makes"
+
+        val request = Request.Builder().url(url).build()
+
+        val client = OkHttpClient()
+
+        client.newCall(request).enqueue(object: Callback{
+            override fun onResponse(response: Response?) {
+                //get json response and cast to string
+                val body = response?.body()?.string()
+                //convert json response to data class
+                val gson = GsonBuilder().create()
+                val holder = gson.fromJson(body, MakeHolder::class.java)
+                //add items from data class to array carMake adapter uses
+                holder.makes.forEach { item -> carMakes.add(item) }
+                //make carMake Adapter aware of updates to list
+                //this needs to be run on UiThread
+                d("olanDebug", "*********************** carMakes updated $carMakes ****************")
+                activity!!.runOnUiThread { carMakeAdapter.notifyDataSetChanged() }
+            }
+
+            override fun onFailure(request: Request?, e: IOException?) {
+                d("olanDebug", "failed rest api call")
+                d("olanDebug", e.toString())
+                carMakes.add("Failed to connect to rest")
+                activity!!.runOnUiThread { carMakeAdapter.notifyDataSetChanged() }
+            }
+
+        })
+    }
+
 }
+//data class to hold json responses to getting car makes
+class MakeHolder(val makes: List<String>)
