fd28cdf664c5a522877d1031e6b72e11dc31849b
olan <olan@olan.com>
2020-03-03 16:19:27 +0000

    added checks to ensure adding activity has all necessary requirements

--- app/src/main/java/com/olan/finalyearproject/ui/fragments/AddActivityFragment.kt
+++ app/src/main/java/com/olan/finalyearproject/ui/fragments/AddActivityFragment.kt
@@ -1,6 +1,5 @@
 package com.olan.finalyearproject.ui.fragments
 
-
 import android.os.Bundle
 import android.util.Log.d
 import androidx.fragment.app.Fragment
@@ -8,29 +7,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import com.google.gson.GsonBuilder
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.olan.finalyearproject.R
 import com.olan.finalyearproject.helpers.CarMakeSpinnerListener
 import com.olan.finalyearproject.helpers.JourneyTypeSpinnerListener
 import com.olan.finalyearproject.helpers.TaskTypeSpinnerListener
-import com.squareup.okhttp.Callback
-import com.squareup.okhttp.OkHttpClient
-import com.squareup.okhttp.Request
-import com.squareup.okhttp.Response
-import org.w3c.dom.Text
-import java.io.IOException
 
-class AddActivityFragment : Fragment() {
+class AddActivityFragment : Fragment(), View.OnClickListener{
 
     //initialise lateinit vars for all view items
     lateinit var taskTypeSpinner: Spinner
     lateinit var journeyTypeTextView: TextView
     lateinit var journeyTypeSpinner: Spinner
-    lateinit var originTextView: TextView
-    lateinit var destinationTextView: TextView
+    lateinit var originTextView: EditText
+    lateinit var destinationTextView: EditText
     lateinit var isElectricSwitch: Switch
     lateinit var carMakeSpinner: Spinner
     lateinit var carModelSpinner: Spinner
+    lateinit var addActivityButton: FloatingActionButton
 
     //initialise lateinit arrays for collections of views
     lateinit var journeyTaskViews: Array<View>
@@ -65,6 +59,7 @@ class AddActivityFragment : Fragment() {
         isElectricSwitch = view.findViewById(R.id.isElectricSwitch)
         carMakeSpinner = view.findViewById(R.id.carMakeSpinner)
         carModelSpinner = view.findViewById(R.id.carModelSpinner)
+        addActivityButton = view.findViewById(R.id.addActivityButton)
 
         //create arrays of items associated with view selections
         journeyTaskViews = arrayOf(journeyTypeTextView, journeyTypeSpinner, originTextView, destinationTextView)
@@ -97,11 +92,52 @@ class AddActivityFragment : Fragment() {
         carModelAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         carModelSpinner.adapter = carModelAdapter
 
-        //set the listener for both spinners to be this class
+        //set the listeners for various views
         taskTypeSpinner.onItemSelectedListener = TaskTypeSpinnerListener(journeyTaskViews)
         journeyTypeSpinner.onItemSelectedListener = JourneyTypeSpinnerListener(
             carJourneyViews, bikeJourneyViews, carMakes, carMakeAdapter, activity!!)
         carMakeSpinner.onItemSelectedListener = CarMakeSpinnerListener(carModels, carModelAdapter, activity!!)
+        addActivityButton.setOnClickListener(this)
+    }
+
+    //functionality for add activity button being pressed
+    //easier to make the fragment itself the listener as there are a lot of different variables at work depending on selection
+    override fun onClick(v: View?) {
+        //what type of task is being created
+        when(taskTypeSpinner.selectedItem.toString()){
+            "JourneyTask" ->{
+                //what type of journey is being created
+                when(journeyTypeSpinner.selectedItem.toString()){
+                    "Bike Journey" ->{
+                        if(
+                            originTextView.text.toString().trim().isNotEmpty() && destinationTextView.text.toString().trim().isNotEmpty()
+                        ){
+                            d("olanDebug", "bike journey - origin: ${originTextView.text} destination: ${destinationTextView.text} isElectric: ${isElectricSwitch.showText}")
+                        } else {
+                            d("olanDebug", "bike journey - origin or destination are blank")
+                        }
+                    }
+                    "Car Journey" ->{
+                        if (
+                            originTextView.text.toString().trim().isNotEmpty() && destinationTextView.text.toString().trim().isNotEmpty() &&
+                                    carMakeSpinner.selectedItem.toString() != "None" && carModelSpinner.selectedItem.toString() != "None"
+
+                        ){
+                            d("olanDebug", "car journey - origin: ${originTextView.text} destination: ${destinationTextView.text} carMake: ${carMakeSpinner.selectedItem} carModel: ${carModelSpinner.selectedItem}")
+                        } else {
+                            d("olanDebug", "car journey - all conditions not met")
+                        }
+                    }
+                    "Transit Journey" ->{
+
+                    }
+                    "Walking Journey" ->{
+
+                    }
+                }
+            }
+            //TODO add handling for other task types
+        }
     }
 
 }
